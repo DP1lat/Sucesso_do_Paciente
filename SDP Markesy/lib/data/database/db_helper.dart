721aa51872f45fb635b072bc0be962ed2205daea
Diego Pilat <dpilat94@gmail.com>
2026-04-04 19:29:05 -0300

    update dbHelper

--- SDP Markesy/lib/data/database/db_helper.dart
+++ SDP Markesy/lib/data/database/db_helper.dart
@@ -1,138 +1,90 @@
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:path/path.dart';
 import 'dart:io';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class DbHelper {
   static Database? _db;
 
-  static Future<Database> get database async {
-    if (_db != null) return _db!;
-    _db = await _initDatabase();
-    return _db!;
+  static final _supabase = Supabase.instance.client;
+
+  static Future<int> inserirPaciente(Map<String, dynamic> dadosPaciente) async {
+    final resp = await _supabase.from('pacientes').insert(dadosPaciente).select('id').single();
+    return resp['id'] as int;
   }
 
-  static Future<Database> _initDatabase() async {
-    sqfliteFfiInit();
-    var databaseFactory = databaseFactoryFfi;
-
-    String userPath = Platform.environment['USERPROFILE'] ?? '';
-    String oneDrivePath = join(userPath, 'OneDrive', 'Documentos', 'ClinicaDados');
-
-    Directory(oneDrivePath).createSync(recursive: true);
-
-    String path = join(oneDrivePath, 'clinica_sucesso.db');
-
-    return await databaseFactory.openDatabase(
-      path,
-      options: OpenDatabaseOptions(
-        version: 1,
-        onCreate: (db, version) async {
-          await db.execute('''
-            CREATE TABLE pacientes (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              nome TEXT,
-              data_nascimento TEXT,
-              telefone TEXT,
-              data_avaliacao TEXT
-            )
-          ''');
-          await db.execute('''
-            CREATE TABLE avaliacoes (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              paciente_id INTEGER,
-              fechou_pacote INTEGER,
-              profissional TEXT,
-              especialidade TEXT,
-              num_sessoes INTEGER,
-              forma_pagamento TEXT,
-              num_parcelas INTEGER,
-              tipo_pagamento TEXT,
-              valor REAL,
-              data_avaliacao TEXT,
-              observacoes TEXT,
-              FOREIGN KEY (paciente_id) REFERENCES pacientes (id)
-            )
-          ''');
-          await db.execute('''
-            CREATE TABLE usuarios (
-              id INTEGER PRIMARY KEY AUTOINCREMENT,
-              login TEXT UNIQUE,
-              senha TEXT,
-              cargo TEXT
-            ) 
-          ''');
-        },
-      ),
-    );
-  }
-
-  static Future<int> inserirPaciente(Map<String, dynamic> dados) async {
-    final db = await database;
-    return await db.insert('pacientes', dados);
-  }
-
-  static Future<int> inserirAvaliacao(Map<String, dynamic> dados) async {
-    final db = await database;
-    return await db.insert('avaliacoes', dados);
-  }
-
-  static Future<List<Map<String, dynamic>>> buscarResumoPaciente(String ordem, {String filtro = ''}) async {
-    final db = await database;
-    String ordemCorrigida = ordem.contains('id') && !ordem.contains('p.') ? ordem.replaceAll('id', 'p.id') : ordem;
-
-    String query =
-        '''
-      SELECT 
-        p.id, p.nome, p.data_avaliacao, p.data_nascimento, p.telefone,
-        a.fechou_pacote, a.profissional, a.especialidade, a.valor,
-        a.tipo_pagamento, a.forma_pagamento, a.num_parcelas, a.num_sessoes, a.observacoes
-      FROM pacientes p
-      LEFT JOIN avaliacoes a ON p.id = a.paciente_id
-      WHERE p.nome LIKE ?
-      ORDER BY $ordemCorrigida
-    ''';
-
-    return await db.rawQuery(query, ['%$filtro%']);
+  static Future<void> atualizarPaciente(int id, Map<String, dynamic> dadosPaciente) async {
+    await _supabase.from('pacientes').update(dadosPaciente).eq('id', id);
   }
 
   static Future<void> excluirPaciente(int id) async {
-    final db = await database;
-    await db.delete('pacientes', where: 'id = ?', whereArgs: [id]);
+    await _supabase.from('avaliacoes').delete().eq('paciente_id', id);
+    await _supabase.from('pacientes').delete().eq('id', id);
   }
 
-  static Future<void> atualizarPaciente(int id, Map<String, dynamic> dados) async {
-    final db = await database;
-    await db.update('pacientes', dados, where: 'id = ?', whereArgs: [id]);
+  static Future<void> inserirAvaliacao(Map<String, dynamic> novaAvaliacao) async {
+    await _supabase.from('avaliacoes').insert(novaAvaliacao);
   }
 
-  static Future<void> atualizarAvaliacao(int pacienteId, Map<String, dynamic> dados) async {
-    final db = await database;
-    await db.update('avaliacoes', dados, where: 'paciente_id = ?', whereArgs: [pacienteId]);
+  static Future<void> atualizarAvaliacao(int pacienteId, Map<String, dynamic> novaAvaliacao) async {
+    await _supabase.from('avalicoes').update(novaAvaliacao).eq('pacienteId', pacienteId);
+  }
+
+  static Future<List<Map<String, dynamic>>> buscarResumoPaciente(String ordem, {String filtro = ''}) async {
+    var query = _supabase.from('pacientes').select(''' 
+      *, 
+      avaliacoes ( * )
+    ''');
+
+    if (filtro.isNotEmpty) {
+      query = query.ilike('nome', '%$filtro%');
+    }
+
+    final List<dynamic> resposta = await query;
+
+    List<Map<String, dynamic>> listaProcessada = resposta.map((item) {
+      var mapPlano = Map<String, dynamic>.from(item);
+
+      var listaAvaliacoes = item['avaliacoes'] as List<dynamic>?;
+      if (listaAvaliacoes != null && listaAvaliacoes.isNotEmpty) {
+        var avaliacao = listaAvaliacoes[0];
+
+        mapPlano['fechou_pacote'] = avaliacao['fechou_pacote'];
+        mapPlano['data_avaliacao'] = avaliacao['data_avaliacao'];
+        mapPlano['profissional'] = avaliacao['profissional'];
+        mapPlano['especialidade'] = avaliacao['especialidade'];
+        mapPlano['valor'] = avaliacao['valor'];
+        mapPlano['observacoes'] = avaliacao['observacoes'];
+      }
+      mapPlano.remove('avalicoes');
+      return mapPlano;
+    }).toList();
+
+    if (ordem == 'nomeASC') {
+      listaProcessada.sort((a, b) => (b['valor'] ?? 0.0).compareTo(a['valor'] ?? 0.0));
+    } else {
+      listaProcessada.sort((a, b) => (b['id'] ?? 0).compareTo(a['id'] ?? 0));
+    }
+
+    return listaProcessada;
   }
 
   static Future<Map<String, dynamic>?> verificarLogin(String usuario, String senha) async {
-    final db = await database;
+    final resposta = await _supabase.from('usuarios').select().eq('login', usuario).eq('senha', senha).maybeSingle();
 
-    List<Map<String, dynamic>> res = await db.query('usuarios', where: 'login = ? AND senha = ?', whereArgs: [usuario, senha]);
-
-    if (res.isNotEmpty) {
-      return res.first;
-    }
-    return null;
+    return resposta;
   }
 
-  static Future<int> inserirUsuario(Map<String, dynamic> dados) async {
-    final db = await database;
-    return await db.insert('usuarios', dados);
+  static Future<void> inserirUsuario(Map<String, dynamic> dadosUsuario) async {
+    await _supabase.from('usuarios').insert(dadosUsuario);
   }
 
   static Future<List<Map<String, dynamic>>> buscarUsuarios() async {
-    final db = await database;
-    return await db.query('usuarios');
+    final List<dynamic> resposta = await _supabase.from('usuarios').select();
+    return List<Map<String, dynamic>>.from(resposta);
   }
 
   static Future<void> excluirUsuario(int id) async {
-    final db = await database;
-    await db.delete('usuarios', where: 'id = ?', whereArgs: [id]);
+    await _supabase.from('usuarios').delete().eq('id', id);
   }
 }
